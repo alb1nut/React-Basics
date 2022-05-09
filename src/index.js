efd4eaea0c1eda29f9916fb24d4a5227d7b53f07
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-05-09 11:02:47 +0000

    added json data for news list

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import ReactDom from 'react-dom';
 
 //Components
 import Header from './components/Header'
+import NewsList from './components/News_List';
+
+
+
 
 const App = () => {
   return(
     <div>
       <Header text ='Logo'/>
-      
+      <NewsList/>
     </div>
   )
 }
